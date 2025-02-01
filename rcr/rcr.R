24c85676f5eb3dd62880161ea2b90d6bd2ea64d6
Timbul Widodo, S.TP <160803827+timbulwidodostp@users.noreply.github.com>
2025-02-01 08:42:58 +0700

    Use rcr (phonTools) With (In) R Software

---
+++ rcr/rcr.R
@@ -0,0 +1,17 @@
+# Olah Data Semarang
+# WhatsApp : +6285227746673
+# IG : @olahdatasemarang_
+# Random coefficients regression (RCR) Use rcr (phonTools) With (In) R Software
+install.packages("phonTools")
+library("phonTools")
+rcr = read.csv("https://raw.githubusercontent.com/timbulwidodostp/rcr_/main/rcr/rcr.csv",sep = ";")
+# Estimation Random coefficients regression (RCR) Use rcr (phonTools) With (In) R Software
+rcr_1 = rcr (Rcr_2 ~ Rcr_1 * Rcr_4, rcr$Group, rcr)
+summary (rcr_1)
+rcr_2 = rcr (Rcr_2 ~ Dummy_, rcr$Group, rcr)
+summary (rcr_2)
+# Random coefficients regression (RCR) Use rcr (phonTools) With (In) R Software
+# Olah Data Semarang
+# WhatsApp : +6285227746673
+# IG : @olahdatasemarang_
+# Finished
